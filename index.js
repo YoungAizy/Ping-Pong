3f3caf7221cae9176503c0f45326530a098a1f70
Ayanda Marotya <ayandamarotya@gmail.com>
2022-08-10 00:20:49 +0200

    configure a public folder

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const app = express();
 const path = require('path')
 
 const port = process.env.PORT;
-app.use(express.static(`${__dirname}`));
+app.use(express.static(`${__dirname}/public`));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 })
 
 app.listen(port);
