810131ca3e01ba13763d7f829285cf13bf6b158d
Ayanda Marotya <ayandamarotya@gmail.com>
2022-08-11 05:43:10 +0200

    point to dist folder

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path')
 const port = process.env.PORT;
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
 })
 
 app.listen(port);
