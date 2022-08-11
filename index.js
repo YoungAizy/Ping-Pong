a7fa988d22ea98711a233f9abfa14902ec0aff9e
Ayanda Marotya <ayandamarotya@gmail.com>
2022-08-11 05:21:59 +0200

    revert

--- index.js
+++ index.js
@@ -5,11 +5,9 @@ const path = require('path')
 const port = process.env.PORT;
 //app.use(express.static(`${__dirname}/public`));
 
-if (process.env.NODE_ENV == "production") {
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, 'dist/index.html'));
-    })
-}
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+})
 
 app.listen(port);
 
