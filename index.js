ab3419425654d28976e1e2d77cd2febe22bf13c5
Ayanda Marotya <ayandamarotya@gmail.com>
2022-08-11 05:18:00 +0200

    add environment check

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const path = require('path')
 const port = process.env.PORT;
 //app.use(express.static(`${__dirname}/public`));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
-})
+if (process.env.NODE_ENV == "production") {
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, 'dist/index.html'));
+    })
+}
 
 app.listen(port);
 
