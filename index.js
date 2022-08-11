767c40fcb3080949eee8b8a82eef38b46ef7cef0
Ayanda Marotya <ayandamarotya@gmail.com>
2022-08-11 05:39:31 +0200

    delete express use static

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 const path = require('path')
 
 const port = process.env.PORT;
-//app.use(express.static(`${__dirname}/public`));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
